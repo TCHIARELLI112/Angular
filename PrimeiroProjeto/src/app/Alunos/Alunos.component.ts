7b65fecd5a698a086690a6be098a95e80cac633f
Thiago Chiarelli <tchiarelli@gmail.com>
2020-08-15 14:33:16 -0300

    angular aula 15/08/2020

--- PrimeiroProjeto/src/app/Alunos/Alunos.component.ts
+++ PrimeiroProjeto/src/app/Alunos/Alunos.component.ts
@@ -1,8 +1,10 @@
 import { Aluno } from './../Models/Aluno';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
-  selector: 'app-Alunos',
+  selector: 'app-alunos',
   templateUrl: './Alunos.component.html',
   styleUrls: ['./Alunos.component.css']
 })
@@ -11,20 +13,49 @@ export class AlunosComponent implements OnInit {
   public titulo = 'Alunos';
 
   public alunoSelecionado: Aluno;
+  public alunoForm: FormGroup;
+  public  modalRef: BsModalRef;
 
 
   public alunos = [
-    {id: 1, nome: 'Diego', sobrenome: 'Cugick', telefone: 33334455},
-    {id: 2, nome: 'Thiago', sobrenome: 'Chiarelli', telefone: 33334455},
-    {id: 3, nome: 'Darlan', sobrenome: 'Silva', telefone: 33334455},
-    {id: 4, nome: 'Gabriel', sobrenome: 'Jose', telefone: 33334455},
-    {id: 5, nome: 'Bradlay', sobrenome: 'Magrão', telefone: 33334455},
+    {id: 1, nome: 'Diego', sobrenome: 'Cugick', telefone: 33334444},
+    {id: 2, nome: 'Thiago', sobrenome: 'Chiarelli', telefone: 33334433},
+    {id: 3, nome: 'Darlan', sobrenome: 'Silva', telefone: 33334422},
+    {id: 4, nome: 'Gabriel', sobrenome: 'Jose', telefone: 33334411},
+    {id: 5, nome: 'Bradlay', sobrenome: 'Magrão', telefone: 33334400},
 
   ];
 
 
+
+  constructor(private formBuilder: FormBuilder, private modalService: BsModalService) {
+
+    this.criarForm();
+
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  criarForm(){
+    this.alunoForm = this.formBuilder.group({
+      nome: ['',Validators.required],
+      sobrenome: ['',Validators.required],
+      telefone: ['',Validators.required]
+    });
+  }
+
+  alunoSubmit(){
+    console.log(this.alunoForm.value);
+  }
+
+  ngOnInit() {
+  }
+
   alunoSelect(aluno: Aluno){
     this.alunoSelecionado = aluno;
+    this.alunoForm.patchValue(aluno);
   }
 
   voltar() {
@@ -32,9 +63,6 @@ export class AlunosComponent implements OnInit {
   }
 
 
-  constructor() { }
 
-  ngOnInit() {
-  }
 
 }
--- PrimeiroProjeto/src/app/professores/professores.component.ts
+++ PrimeiroProjeto/src/app/professores/professores.component.ts
@@ -1,5 +1,7 @@
 import { Professor } from './../Models/Professor';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-professores',
@@ -11,6 +13,8 @@ export class ProfessoresComponent implements OnInit {
 
   public titulo = 'Professores';
   public professorSelecionado: Professor;
+  public professorForm: FormGroup;
+  public modalRef: BsModalRef;
 
   public professores = [
     {id: 1 , nome: 'Luiz' , disciplina: 'Delphi' },
@@ -19,10 +23,31 @@ export class ProfessoresComponent implements OnInit {
     {id: 4 , nome: 'Pedro' , disciplina: 'Banco de Dados' },
 
   ];
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private modalService: BsModalService) {
+
+    this.criarForm();
+
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  criarForm(){
+    this.professorForm = this.formBuilder.group({
+      nome: ['',Validators.required],
+      disciplina: ['',Validators.required],
+
+    });
+  }
+
+  professorSubmit(){
+    console.log(this.professorForm.value);
+  }
 
   profesorSelect(professor: Professor){
     this.professorSelecionado = professor;
+    this.professorForm.patchValue(professor);
   }
 
   voltar() {
